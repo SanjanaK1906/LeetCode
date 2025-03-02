90f50333ecb9f84d674f42ecbd0ddc2eed5e9b5d
Sanjana Kumbhar <120375747+SanjanaK1906@users.noreply.github.com>
2025-03-02 23:38:31 +0530

    Create 1683_Invalid Tweets.sql

---
+++ DBMS/1683_Invalid Tweets.sql
@@ -0,0 +1,24 @@
+/*Example 1:
+
+Input: 
+Tweets table:
++----------+-----------------------------------+
+| tweet_id | content                           |
++----------+-----------------------------------+
+| 1        | Let us Code                       |
+| 2        | More than fifteen chars are here! |
++----------+-----------------------------------+
+Output: 
++----------+
+| tweet_id |
++----------+
+| 2        |
++----------+
+Explanation: 
+Tweet 1 has length = 11. It is a valid tweet.
+Tweet 2 has length = 33. It is an invalid tweet.
+*/
+# Write your MySQL query statement below
+select tweet_id
+from Tweets
+where length(content)>15;
